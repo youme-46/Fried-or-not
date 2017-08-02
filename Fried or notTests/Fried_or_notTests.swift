03cd35849bd9e677de2fc0e91551483cc260a4e4
畑中佑太 <Yuta@hatanakayuuta-no-macbook-pro.local>
2017-08-02 15:18:37 +0900

    Initial Commit

---
+++ Fried or notTests/Fried_or_notTests.swift
@@ -0,0 +1,36 @@
+//
+//  Fried_or_notTests.swift
+//  Fried or notTests
+//
+//  Created by 畑中佑太 on 2017/08/02.
+//  Copyright © 2017年 欅坂46. All rights reserved.
+//
+
+import XCTest
+@testable import Fried_or_not
+
+class Fried_or_notTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
